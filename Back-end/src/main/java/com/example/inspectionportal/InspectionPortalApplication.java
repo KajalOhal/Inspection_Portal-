d872ce36d174c114ffdd6299970dccd14afbb910
Kajal <ohalkajal123@gmail.com>
2023-12-20 12:44:27 +0530

    back end

---
+++ Back-end/src/main/java/com/example/inspectionportal/InspectionPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.inspectionportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectionPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspectionPortalApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/com/example/inspectionportal/InspectionPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inspectionportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspectionPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
